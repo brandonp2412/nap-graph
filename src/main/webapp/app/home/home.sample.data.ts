d42d4afe1b72ba274596d4f87a8118f477885913
Brandon Presley <brandonp2412@gmail.com>
2018-04-28 14:37:32 +1200

    Sample data complies with lint and added clarification of units on NapDialog

--- src/main/webapp/app/home/home.sample.data.ts
+++ src/main/webapp/app/home/home.sample.data.ts
@@ -1,22 +1,21 @@
 export const SAMPLE_DATE_DURATION = [ {
-    "localDate" : "2018-04-11",
-    "totalDuration" : 12,
+    'localDate' : '2018-04-11',
+    'totalDuration' : 12,
 }, {
-    "localDate" : "2018-04-14",
-    "totalDuration" : 5,
+    'localDate' : '2018-04-14',
+    'totalDuration' : 5,
 }, {
-    "localDate" : "2018-04-22",
-    "totalDuration" : 7,
+    'localDate' : '2018-04-22',
+    'totalDuration' : 7,
 } ];
 
-
 export const SAMPLE_DURATION_RATING = [ {
-    "duration" : 5,
-    "averageRating" : 6,
+    'duration' : 5,
+    'averageRating' : 6,
 }, {
-    "duration" : 7,
-    "averageRating" : 9,
+    'duration' : 7,
+    'averageRating' : 9,
 }, {
-    "duration" : 12,
-    "averageRating" : 1,
+    'duration' : 12,
+    'averageRating' : 1,
 } ];
